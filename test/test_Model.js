dbc0068d4e9828a6aef80f58eef3515ee703fd01
Duncan <dncn.mcdougall@gmail.com>
2016-10-15 20:51:40 +0200

    Adds empty descriptions to the model test.

---
+++ test/test_Model.js
@@ -0,0 +1,24 @@
+/*eslint-env jasmine */
+
+describe('Model: The model returned from the crerator. Used to process state.', function() {
+    describe('reduce: Perform the named action on the given state and return the new state.', function() {
+    });
+    describe('request: Perform the named request using the given state and return the result.', function() {
+    });
+    describe('listActions: Lists all the actions registered on this model, recursively.', function() {
+    });
+    describe('listRequests: Lists all the requests registered on this model, recursively.', function() {
+    });
+    describe('createEmpty: Creates an empty state representing this model.', function() {
+    });
+    describe('State (request): Returns the state representing this model from within the given state.', function() {
+    });
+    describe('Set[PropertyName] (action): Sets the named property to the given value and returnes the new state.', function() {
+    });
+    describe('Add[ChildName] (action): Adds an empty instance of the child, under the given key, and returns the new state.', 
+        function() {
+        });
+    describe('Available[ChildName] (request): Returns the available key value for the given child collection in the state.', 
+        function() {
+        });
+});
